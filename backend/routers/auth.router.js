11f2788856e5f8ca234d64158290d700336b075b
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2025-04-02 19:57:08 +0100

    Main: finished admin midleware

--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -3,6 +3,7 @@ import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
